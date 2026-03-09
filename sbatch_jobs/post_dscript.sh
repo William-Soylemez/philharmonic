9549fcba7f8b25a440323162146ecfd74fc2695c
Henry-Cat <57220048+William-Soylemez@users.noreply.github.com>
2026-03-09 00:58:10 -0400

    new post dscript

--- sbatch_jobs/post_dscript.sh
+++ sbatch_jobs/post_dscript.sh
@@ -1,1 +1,25 @@
-cat dscript_work/predictions_task_*.positive.tsv > _network.positive.tsv
+#!/bin/bash
+#SBATCH --job-name=post_dscript
+#SBATCH --output=post_dscript_%j.out
+#SBATCH --error=post_dscript_%j.err
+#SBATCH --time=04:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=144
+#SBATCH --mem=0
+
+export SPECIES=$1
+
+# Load necessary modules and activate virtual environment
+cd $SCRATCH
+module load gcc cuda python3
+source venv/bin/activate
+
+# Environment variables
+export SNAKE="snakemake --snakefile Snakefile_slurm --configfile configs/config_slurm.yml --cores 144 --rerun-incomplete"
+export OPENAI_API_KEY=""
+
+# Run the Snakemake workflow
+cd $SCRATCH/philharmonic
+cat ${SPECIES}_results/dscript_work/predictions_task_*.positive.tsv > ${SPECIES}_results/${SPECIES}_network.positive.tsv
+$SNAKE ${SPECIES}_results/${SPECIES}.zip
